4e86ad976f71f7c8b4ed1070f4c89b13877db41a
play <t.pauljabez@gamil.com>
2023-06-26 11:12:10 +0530

    Materials

--- Gun Mechanics/Assets/Script/Rotate.cs
+++ Gun Mechanics/Assets/Script/Rotate.cs
@@ -7,15 +7,15 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 50.0f; 
+  
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
     }
 }
