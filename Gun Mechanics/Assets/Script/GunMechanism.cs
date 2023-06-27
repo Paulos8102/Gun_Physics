da717a102fbc0db0084dc343305db495997bd768
play <t.pauljabez@gamil.com>
2023-06-28 03:53:06 +0530

    Particle System

--- Gun Mechanics/Assets/Script/GunMechanism.cs
+++ Gun Mechanics/Assets/Script/GunMechanism.cs
@@ -7,14 +7,38 @@ public class GunMechanism : MonoBehaviour
 
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
+
     [SerializeField] public float bulletSpeed = 10.0f;
+
+    public ParticleSystem muzzleFlash;
+
+    private bool isFiring = false;
 
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !isFiring)
         {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            StartCoroutine(FireBullet());
         }
+    }
+
+    private IEnumerator FireBullet()
+    {
+        isFiring = true;
+
+        var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+
+        muzzleFlash.Play();
+
+        // Get the duration of the muzzle flash
+        var muzzleFlashDuration = muzzleFlash.main.duration;
+
+        // Wait for the duration of the muzzle flash
+        yield return new WaitForSeconds(muzzleFlashDuration);
+
+        muzzleFlash.Stop();
+        isFiring = false;
     }
 }
