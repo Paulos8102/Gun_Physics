28ba20b42b168e6b4b1bd57ffbb8346769113d88
play <t.pauljabez@gamil.com>
2023-06-28 18:13:26 +0530

    AudioSource

--- Gun Mechanics/Assets/Script/GunMechanism.cs
+++ Gun Mechanics/Assets/Script/GunMechanism.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 
 public class GunMechanism : MonoBehaviour
 {
-
+    //Gun objects
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
-    public float fireRate = 1f;
+    [SerializeField] public float fireRate = 1f;
 
     [SerializeField] public float bulletSpeed = 10.0f;
 
     public ParticleSystem muzzleFlash;
 
+    //Firing Check
     private bool isFiring = false;
-
     private float nextTimeToFire = 0f;
+
+    //Objects to implement reload time
+    public int maxAmmo = 2;
+    private int currentAmmo;
+    public float reloadTime = 2f;
+    private bool isReloading = false;
+
+    public Animator anim;
+
+    //Audio effects
+    [SerializeField] private AudioSource reloadSound;
+    [SerializeField] private AudioSource shootSound; 
+
+
+    public void Start()
+    {
+        if(currentAmmo ==-1)
+            currentAmmo = maxAmmo;
+    }
+
+    public void OnEnbale()
+    {
+        isReloading = false;
+        anim.SetBool("Reloading", false);
+    }
 
     public void Update()
     {
+        if ( isReloading )
+            return;
+
+        if ( currentAmmo <= 0 )
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if ( Input.GetKeyDown(KeyCode.Space) && !isFiring && Time.time  >= nextTimeToFire )
         {
             nextTimeToFire = Time.time + 1f / fireRate;
@@ -26,15 +60,37 @@ public class GunMechanism : MonoBehaviour
         }
     }
 
+    public IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading");
+
+        anim.SetBool("Reloading", true);
+
+        reloadSound.Play();
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+
+        anim.SetBool("Reloading", false);
+
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     private IEnumerator FireBullet()
     {
         isFiring = true;
+
+        currentAmmo--;
 
         var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
 
         bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
 
         muzzleFlash.Play();
+        shootSound.Play();
 
         // Get the duration of the muzzle flash
         var muzzleFlashDuration = muzzleFlash.main.duration;
